effbe9685f8727cb820292c4ac707ad6355f4c2c
MalikGB <Maliktifah378@gmail.com>
2022-03-21 12:17:45 -0500

    Added updated tile map and fixed jump bug

--- quantumQuarrel/Resources/Background.tsx
+++ quantumQuarrel/Resources/Background.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Background" tilewidth="16" tileheight="16" tilecount="8040" columns="120">
+<tileset version="1.8" tiledversion="1.8.2" name="Background" tilewidth="32" tileheight="32" tilecount="8040" columns="60">
  <image source="background.png" width="1920" height="1080"/>
 </tileset>
